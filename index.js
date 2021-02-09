3f53c892c937559af4a017d4e28688ce07a4ca5e
ArizonaMangoJuice <isaelwebdeveloper@gmail.com>
2021-02-09 00:35:41 -0700

    added error bubble when nnot input correct email

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// console.log(document.getElementsByClassName("email")[0])
 let error = false;
 let isEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
@@ -8,15 +7,18 @@ document
         let value = event.target.value;
         let error = document.getElementsByClassName("error")[0];
         let email = document.getElementsByClassName("email-container")[0];
+        let exError = document.getElementsByClassName("ex-error")[0];
 
         if(!isEmail.test(value)) {
             error.classList.remove("hidden");
             email.classList.add("error-outline");
+            exError.classList.remove("hidden");
         }
 
         if(isEmail.test(value)) {
             error.classList.add("hidden");
             email.classList.remove("error-outline");
+            exError.classList.add("hidden");
         }
 });
 
